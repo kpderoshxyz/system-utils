a7f0b0b42f25e63a4e2b0fc5691c040a6f069abd
Kyle Derosha <kpderosh@gmail.com>
2024-08-13 14:35:20 -0400

    Updating requirements

--- cmd/mask/main.go
+++ cmd/mask/main.go
@@ -3,5 +3,7 @@ package main
 
 func main(){
     // Read in standard input and write mask to standard output
+    // Mask output format -> stdin[0]-stdin[3] rest of stdin is '*' for each character. suffix output with length of stdin.
+    // Ex: randomsecretissecret -> rand****************[57]
 
 }
