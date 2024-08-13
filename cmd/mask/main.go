52e72ae2156222da9e218c60448f3a5e2a92669f
Kyle Derosha <kpderosh@gmail.com>
2024-08-13 19:36:28 -0400

    Adding in implementation

--- cmd/mask/main.go
+++ cmd/mask/main.go
@@ -1,9 +1,26 @@
 package main
 
+import (
+    "bufio"
+    "strings"
+    "os"
+    "fmt"
+)
 
+
+// Read in standard input and write mask to standard output
+// Mask output format -> stdin[0]-stdin[3] rest of stdin is '*' for each character. suffix output with length of stdin.
+// Ex: randomsecretissecret -> rand****************[57]
 func main(){
-    // Read in standard input and write mask to standard output
-    // Mask output format -> stdin[0]-stdin[3] rest of stdin is '*' for each character. suffix output with length of stdin.
-    // Ex: randomsecretissecret -> rand****************[57]
+    stdInReader := bufio.NewReader(os.Stdin)
+    text, err := stdInReader.ReadString('\n')
+    if err != nil {
+        panic(err)
+    }
 
+    if len(text) >= 3 {
+        fmt.Printf("%s%s[%d]\n", text[:3], strings.Repeat("*", len(text) - 3), len(text))
+    } else {
+        fmt.Println(text)
+    }
 }
