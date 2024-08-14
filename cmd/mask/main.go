94d9231295a3b613ff60176f72d078b8935824bf
Kyle Derosha <kpderosh@gmail.com>
2024-08-13 20:57:27 -0400

    Updating the comment description of the application again

--- cmd/mask/main.go
+++ cmd/mask/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 // Read in standard input and write mask to standard output
-// Mask output format -> stdin[0]-stdin[3] rest of stdin is '*' for each character. suffix output with length of stdin.
+// Mask output format -> stdin[0]-stdin[2] rest of stdin is '*' for each character. suffix output with length of stdin.
 // Ex: testthis -> tes*****[8]
 func main(){
     stdInReader := bufio.NewReader(os.Stdin)
